da61920a98cc97de4e8d9e98771d410bc05ba060
michaelwright235 <maiklpolkovnikov@gmail.com>
2024-03-02 18:47:50 +0300

    Imprement encoding of NIB Archives

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@
 
 use std::{
     fs::File,
-    io::{BufReader, Cursor, Read, Seek, SeekFrom},
+    io::{BufReader, BufWriter, Cursor, Read, Seek, SeekFrom, Write},
 };
 
 const MAGIC_BYTES: &[u8; 10] = b"NIBArchive";
 type VarInt = i32;
 
-/// Variants of error that may occur during parsing a NIB Archive.
+/// Variants of error that may occur during encoding/decoding a NIB Archive.
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    /// An IO error that may occur during opening/reading a file.
+    /// An IO error that may occur during opening/reading/writing a file.
     #[error("IOError: {0}")]
     IOError(#[from] std::io::Error),
 
-    /// A format error that may occur during parsing a NIB Archive.
+    /// A format error that may occur only during decoding a NIB Archive.
     /// Usually it indicates a malformed file.
     #[error("NIB Archive format error: {0}")]
     FormatError(String),
@@ -23,7 +23,7 @@ pub enum Error {
 
 impl From<std::string::FromUtf8Error> for Error {
     fn from(value: std::string::FromUtf8Error) -> Self {
-        Self::FormatError(format!("unable to parse UTF-8 string. {value}"))
+        Self::FormatError(format!("Unable to parse UTF-8 string. {value}"))
     }
 }
 
@@ -40,6 +40,23 @@ pub struct Header {
     pub offset_values: u32,
     pub class_name_count: u32,
     pub offset_class_names: u32,
+}
+
+impl Default for Header {
+    fn default() -> Self {
+        Self {
+            format_version: 1,
+            coder_version: 9,
+            object_count: Default::default(),
+            offset_objects: Default::default(),
+            key_count: Default::default(),
+            offset_keys: Default::default(),
+            value_count: Default::default(),
+            offset_values: Default::default(),
+            class_name_count: Default::default(),
+            offset_class_names: Default::default(),
+        }
+    }
 }
 
 impl Header {
@@ -64,27 +81,27 @@ impl Header {
             offset_class_names: values[9],
         })
     }
+
+    pub(crate) fn to_bytes(&self) -> Vec<u8> {
+        let mut result = Vec::with_capacity(40);
+        result.extend_from_slice(&self.format_version.to_le_bytes());
+        result.extend_from_slice(&self.coder_version.to_le_bytes());
+        result.extend_from_slice(&self.object_count.to_le_bytes());
+        result.extend_from_slice(&self.offset_objects.to_le_bytes());
+        result.extend_from_slice(&self.key_count.to_le_bytes());
+        result.extend_from_slice(&self.offset_keys.to_le_bytes());
+        result.extend_from_slice(&self.value_count.to_le_bytes());
+        result.extend_from_slice(&self.offset_values.to_le_bytes());
+        result.extend_from_slice(&self.class_name_count.to_le_bytes());
+        result.extend_from_slice(&self.offset_class_names.to_le_bytes());
+        result
+    }
 }
 
 /// Represents a single object of a NIB Archive.
 ///
 /// An object contains an index of a representing class name, the first index of
 /// a value and the count of all values.
-///
-/// The following example shows a proccess of decoding a object:
-/// ```
-/// let archive = ...;
-/// let object: Object = archive.objects().get(0)?;
-/// let class_name: ClassName = archive.class_names().get(object.class_name_index() as usize)?;
-/// let values: Vec<&Value> = Vec::with_capacity(object.value_count() as usize);
-/// for i in object.values_index()..object.values_index()+object.value_count() {
-///     values.push(archive.values().get(i)?);
-/// }
-///
-/// println!("Class name: {classname:?}");
-/// println!("Values:");
-/// println!("{values:#?}");
-/// ```
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Object {
     class_name_index: VarInt,
@@ -99,6 +116,18 @@ impl Object {
             values_index: decode_var_int(&mut reader)?,
             value_count: decode_var_int(&mut reader)?,
         })
+    }
+
+    pub(crate) fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = encode_var_int(self.class_name_index);
+        bytes.append(&mut encode_var_int(self.values_index));
+        bytes.append(&mut encode_var_int(self.value_count));
+        bytes
+    }
+
+    /// Creates a new NIB Archive object.
+    pub fn new(class_name_index: VarInt, values_index: VarInt, value_count: VarInt) -> Self {
+        Self { class_name_index, values_index, value_count }
     }
 
     /// Returns an index of a [ClassName] that describes the current object.
@@ -138,7 +167,7 @@ impl Object {
     }
 }
 
-/// Represents any object value
+/// Represents any object value.
 #[derive(Debug, Clone, PartialEq)]
 pub enum ValueVariant {
     Int8(i8),
@@ -222,6 +251,63 @@ impl Value {
         Ok(Self { key_index, value })
     }
 
+    pub(crate) fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = encode_var_int(self.key_index);
+
+        match &self.value {
+            ValueVariant::Int8(v) => {
+                bytes.push(0);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+            ValueVariant::Int16(v) => {
+                bytes.push(1);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+            ValueVariant::Int32(v) => {
+                bytes.push(2);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+            ValueVariant::Int64(v) => {
+                bytes.push(3);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+            ValueVariant::Bool(v) => {
+                if !v {
+                    bytes.push(4);
+                } else {
+                    bytes.push(5);
+                }
+            }
+            ValueVariant::Float(v) => {
+                bytes.push(6);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+            ValueVariant::Double(v) => {
+                bytes.push(7);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+            ValueVariant::Data(v) => {
+                bytes.push(8);
+                bytes.append(&mut encode_var_int(v.len() as i32));
+                bytes.extend_from_slice(v);
+            }
+            ValueVariant::Nil => {
+                bytes.push(9);
+            }
+            ValueVariant::ObjectRef(v) => {
+                bytes.push(10);
+                bytes.extend_from_slice(&v.to_le_bytes());
+            }
+        }
+
+        bytes
+    }
+
+    /// Creates a new NIB Archive value.
+    pub fn new(key_index: VarInt, value: ValueVariant) -> Self {
+        Self { key_index, value }
+    }
+
     /// Returns an index to a key with value's name.
     pub fn key_index(&self) -> VarInt {
         self.key_index
@@ -272,6 +358,24 @@ impl ClassName {
         })
     }
 
+    pub(crate) fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = encode_var_int(self.name.len() as i32 + 1);
+        bytes.append(&mut encode_var_int(
+            self.fallback_classes_indeces.len() as i32
+        ));
+        for cls in &self.fallback_classes_indeces {
+            bytes.extend_from_slice(&cls.to_le_bytes());
+        }
+        bytes.extend_from_slice(self.name.as_bytes());
+        bytes.push(0x00);
+        bytes
+    }
+
+    /// Creates a new NIB Archive class name.
+    pub fn new(name: String, fallback_classes_indeces: Vec<i32>) -> Self {
+        Self { name, fallback_classes_indeces }
+    }
+
     /// Returns the name of a class.
     pub fn name(&self) -> &str {
         &self.name
@@ -305,7 +409,7 @@ macro_rules! check_position {
     ($reader:ident, $offset:expr, $err:literal) => {
         if $reader.stream_position()? != $offset as u64 {
             return Err(Error::FormatError(format!(
-                "expected {} offset at {} - got {}",
+                "Expected {} offset at {} - got {}",
                 $err,
                 $reader.stream_position()?,
                 $offset
@@ -314,7 +418,7 @@ macro_rules! check_position {
     };
 }
 
-/// NIB Archive decoder.
+/// NIB Archive encoder/decoder.
 ///
 /// Look at module docs for more info.
 #[derive(Debug, Clone, PartialEq)]
@@ -327,20 +431,41 @@ pub struct NIBArchive {
 }
 
 impl NIBArchive {
-    /// Reads a NIB Archive from a given file.
+
+    /// Creates a new NIB Archive from `objects`, `keys`, `values` and `class_names`.
+    ///
+    /// This method **does not** check the input values. For example, a situation when an object's
+    /// key index is out of bounds of the `keys` parameter is unchecked. To ensure properly formed
+    /// archive you should check those yourself.
+    pub fn new(
+        objects: Vec<Object>,
+        keys: Vec<String>,
+        values: Vec<Value>,
+        class_names: Vec<ClassName>,
+    ) -> Self {
+        Self {
+            header: Header::default(),
+            objects,
+            keys,
+            values,
+            class_names,
+        }
+    }
+
+    /// Reads and decodes a NIB Archive from a given file.
     pub fn from_file<P: AsRef<std::path::Path>>(path: P) -> Result<Self, Error> {
         let file = File::open(path)?;
         let mut reader = BufReader::new(file);
         Self::from_reader(&mut reader)
     }
 
-    /// Reads a NIB Archive from a given slice of byte.
+    /// Reads and decodes a NIB Archive from a given slice of byte.
     pub fn from_bytes<B: AsRef<[u8]>>(bytes: B) -> Result<Self, Error> {
         let mut cursor = Cursor::new(bytes);
         Self::from_reader(&mut cursor)
     }
 
-    /// Reads a NIB Archive from a given reader.
+    /// Reads and decodes a NIB Archive from a given reader.
     pub fn from_reader<T: Read + Seek>(mut reader: &mut T) -> Result<Self, Error> {
         reader.seek(SeekFrom::Start(0))?;
 
@@ -348,9 +473,7 @@ impl NIBArchive {
         let mut magic_bytes = [0; 10];
         reader.read_exact(&mut magic_bytes)?;
         if &magic_bytes != MAGIC_BYTES {
-            return Err(Error::FormatError(
-                "magic bytes don't match".into(),
-            ));
+            return Err(Error::FormatError("Magic bytes don't match".into()));
         }
 
         // Parse header
@@ -361,11 +484,11 @@ impl NIBArchive {
         let mut objects = Vec::with_capacity(header.object_count as usize);
         for _ in 0..header.object_count {
             let obj = Object::try_from_reader(&mut reader)?;
-            if (obj.values_index + obj.value_count) as u32 >= header.object_count {
-                return Err(Error::FormatError("value index out of bounds".into()));
+            if (obj.values_index + obj.value_count) as u32 > header.value_count {
+                return Err(Error::FormatError("Value index out of bounds".into()));
             }
-            if obj.class_name_index as u32 >= header.class_name_count {
-                return Err(Error::FormatError("class name index out of bounds".into()));
+            if obj.class_name_index as u32 > header.class_name_count {
+                return Err(Error::FormatError("Class name index out of bounds".into()));
             }
             objects.push(obj);
         }
@@ -386,8 +509,8 @@ impl NIBArchive {
         let mut values = Vec::with_capacity(header.value_count as usize);
         for _ in 0..header.value_count {
             let val = Value::try_from_reader(&mut reader)?;
-            if val.key_index as u32 >= header.key_count {
-                return Err(Error::FormatError("key index out of bounds".into()));
+            if val.key_index as u32 > header.key_count {
+                return Err(Error::FormatError("Key index out of bounds".into()));
             }
             values.push(val);
         }
@@ -398,8 +521,10 @@ impl NIBArchive {
         for _ in 0..header.class_name_count {
             let cls = ClassName::try_from_reader(&mut reader)?;
             for index in &cls.fallback_classes_indeces {
-                if *index as u32 >= header.class_name_count {
-                    return Err(Error::FormatError("class name (fallback class) index out of bounds".into()));
+                if *index as u32 > header.class_name_count {
+                    return Err(Error::FormatError(
+                        "Class name (fallback class) index out of bounds".into(),
+                    ));
                 }
             }
             class_names.push(cls);
@@ -414,14 +539,101 @@ impl NIBArchive {
         })
     }
 
+    /// Encodes an archive and saves it to a file with a given path.
+    pub fn to_file<P: AsRef<std::path::Path>>(&mut self, path: P) -> Result<(), Error> {
+        let file = File::create(path)?;
+        let mut reader = BufWriter::new(file);
+        self.to_writer(&mut reader)
+    }
+
+    /// Encodes an archive and returns a vector of bytes.
+    pub fn to_bytes(&mut self) -> Vec<u8> {
+        let mut cursor = Cursor::new( Vec::with_capacity(1024) );
+        self.to_writer(&mut cursor).unwrap(); // should be safe since we're writing into a vector
+        cursor.into_inner()
+    }
+
+    /// Encodes an archive using a given writer.
+    pub fn to_writer<T: Write>(&mut self, writer: &mut T) -> Result<(), Error> {
+        // Each objects contains 3 fields with up to 2 bytes VarInt
+        let mut objects_bytes = Vec::with_capacity(self.objects.len() * 3 * 2);
+        for obj in &self.objects {
+            objects_bytes.append(&mut obj.to_bytes());
+        }
+
+        // Let's estimate the average key length as 16 symbols
+        let mut keys_bytes = Vec::with_capacity(self.keys.len() * (16 + 2));
+        for key in &self.keys {
+            keys_bytes.append(&mut encode_var_int(key.len() as i32));
+            keys_bytes.extend_from_slice(key.as_bytes());
+        }
+
+        let mut values_bytes = Vec::with_capacity(self.values.len() * (8 + 2));
+        for val in &self.values {
+            values_bytes.append(&mut val.to_bytes());
+        }
+
+        let mut classes_bytes = Vec::with_capacity(self.class_names.len() * 16);
+        for cls in &self.class_names {
+            classes_bytes.append(&mut cls.to_bytes());
+        }
+
+        let header = Header {
+            format_version: self.header.format_version,
+            coder_version: self.header.coder_version,
+            object_count: self.objects.len() as u32,
+            offset_objects: 50,
+            key_count: self.keys.len() as u32,
+            offset_keys: (50 + objects_bytes.len()) as u32,
+            value_count: self.values.len() as u32,
+            offset_values: (50 + objects_bytes.len() + keys_bytes.len()) as u32,
+            class_name_count: self.class_names.len() as u32,
+            offset_class_names: (50 + objects_bytes.len() + keys_bytes.len() + values_bytes.len())
+                as u32,
+        };
+
+        writer.write_all(MAGIC_BYTES)?;
+        writer.write_all(&header.to_bytes())?;
+        writer.write_all(&objects_bytes)?;
+        writer.write_all(&keys_bytes)?;
+        writer.write_all(&values_bytes)?;
+        writer.write_all(&classes_bytes)?;
+        writer.flush()?;
+        self.header = header;
+
+        Ok(())
+    }
+
     /// Returns a reference to a [Header] that describes the current archive.
+    ///
+    /// If you're creating a new archive with the [new](Self::new()) method,
+    /// the header is going to be empty, except for `format_version` and `coder_version`
+    /// fields. It will be filled after encoding your archive.
+    ///
+    /// If you read an archive and then change its properties,
+    /// the header won't reflect these changes until the next encoding.
     pub fn header(&self) -> &Header {
         &self.header
     }
 
-    /// Returns an array of archive's [objects](Object).
+    /// Set header's format version field.
+    pub fn set_format_version(&mut self, value: u32) {
+        self.header.format_version = value;
+    }
+
+    /// Set header's coder version field.
+    pub fn set_coder_version(&mut self, value: u32) {
+        self.header.coder_version = value;
+    }
+
+    /// Returns a reference to a vector of archive's [objects](Object).
     pub fn objects(&self) -> &[Object] {
         &self.objects
+    }
+
+    /// Returns a mutable reference to a vector of archive's [objects](Object).
+    pub fn objects_mut(&mut self) -> &mut Vec<Object> {
+        &mut self.objects
     }
 
     /// Returns an array of archive's keys.
@@ -429,16 +641,32 @@ impl NIBArchive {
         &self.keys
     }
 
-    /// Returns an array of archive's [values](Value).
+    /// Returns a reference to a vector of archive's keys.
+    pub fn keys_mut(&mut self) -> &mut Vec<String> {
+        &mut self.keys
+    }
+
+    /// Returns a reference to a vector of archive's [values](Value).
     pub fn values(&self) -> &[Value] {
         &self.values
     }
 
-    /// Returns an array of archive's [class names](ClassName).
+    /// Returns a mutable reference to a vector of archive's [values](Value).
+    pub fn values_mut(&mut self) -> &mut Vec<Value> {
+        &mut self.values
+    }
+
+    /// Returns a reference to a vector of archive's [class names](ClassName).
     pub fn class_names(&self) -> &[ClassName] {
         &self.class_names
     }
 
+    /// Returns a mutable reference to a vector of archive's [class names](ClassName).
+    pub fn class_names_mut(&mut self) -> &mut Vec<ClassName> {
+        &mut self.class_names
+    }
+
+    /// Consumes itself and returns returns a unit of objects, keys, values and class names.
     pub fn into_inner(self) -> (Vec<Object>, Vec<String>, Vec<Value>, Vec<ClassName>) {
         (self.objects, self.keys, self.values, self.class_names)
     }
@@ -460,4 +688,36 @@ fn decode_var_int<T: Read + Seek>(reader: &mut T) -> Result<VarInt, Error> {
         }
     }
     Ok(result)
+}
+
+/// Encodes an i32 into a variable integer bytes.
+fn encode_var_int(mut value: VarInt) -> Vec<u8> {
+    let mut number_of_bytes = 0;
+    let mut _v = value;
+    loop {
+        number_of_bytes += 1;
+        _v >>= 7;
+        if _v == 0 {
+            break;
+        }
+    }
+
+    let mut offset = 0;
+    let mut bytes = vec![0; number_of_bytes];
+
+    while offset < number_of_bytes {
+        let digit: u8 = (0x7f & value) as u8;
+        value >>= 7;
+
+        let is_last_digit = value == 0;
+
+        bytes[offset] = digit | if is_last_digit { 0x80 } else { 0 };
+        offset += 1;
+
+        if is_last_digit {
+            break;
+        }
+    }
+
+    bytes
 }
