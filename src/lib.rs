ffbbd240cdc7ee07be06d30c28ff7407a2f64058
michaelwright235 <maiklpolkovnikov@gmail.com>
2024-03-03 02:33:40 +0300

    Remove thiserror dependency

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,31 @@ const MAGIC_BYTES: &[u8; 10] = b"NIBArchive";
 type VarInt = i32;
 
 /// Variants of error that may occur during encoding/decoding a NIB Archive.
-#[derive(thiserror::Error, Debug)]
+#[derive(Debug)]
 pub enum Error {
     /// An IO error that may occur during opening/reading/writing a file.
-    #[error("IOError: {0}")]
-    IOError(#[from] std::io::Error),
+    IOError(std::io::Error),
 
     /// A format error that may occur only during decoding a NIB Archive.
     /// Usually it indicates a malformed file.
-    #[error("NIB Archive format error: {0}")]
     FormatError(String),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::IOError(e) => f.write_fmt(format_args!("IOError: {e}")),
+            Error::FormatError(e) => f.write_fmt(format_args!("NIB Archive format error: {e}")),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IOError(value)
+    }
 }
 
 impl From<std::string::FromUtf8Error> for Error {
